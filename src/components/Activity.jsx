87bb64a1b8e693c9c62b9a1d38756390f7727041
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-16 02:56:32 +0300

    Mapping through the json file

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Activity(){
+export default function Activity({title}){
     return <div className="activity">
     <img className="icon" src="./images/icon-work.svg" alt="icon-briefcase" />
         <div className="work">
             <div className="title">
-                <h2>Work</h2>
+                <h2>{title}</h2>
                 <img src="./images/icon-ellipsis.svg" alt="icon-ellipsis" />
             </div>
             <div className="content">
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Heading from "./Heading";
 import Activity from "./Activity";
+import data from "../data.json";
 
+ data = [data];
+ 
 export default function App() {
   return (
     <div className="container">
       <Heading />
-      <Activity/>
+      {data.map((item, index)=>{
+        return <Activity key={index} title={item[index].title}/>
+      })}
     </div>
   );
 }
