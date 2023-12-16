aa367a434ac3e4b796f0fcfcb8faae0daa50ca0d
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-16 22:10:04 +0300

    Fix bg color issue and images issue

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -5,6 +5,7 @@ import studyIcon from "../images/icon-study.svg";
 import exerciseIcon from "../images/icon-exercise.svg";
 import socialIcon from "../images/icon-social.svg";
 import selfCareIcon from "../images/icon-self-care.svg";
+import ellipsis from "../images/icon-ellipsis.svg";
 
 const icons = [
   workIcon,
@@ -15,14 +16,23 @@ const icons = [
   selfCareIcon,
 ];
 
+const bgColors = [
+  "hsl(15, 100%, 70%)",
+  "hsl(195, 74%, 62%)",
+  "hsl(348, 100%, 68%)",
+  "hsl(145, 58%, 55%)",
+  "hsl(264, 64%, 52%)",
+  "hsl(43, 84%, 65%)",
+];
+
 export default function Activity({ title, current, previous, id }) {
   return (
-    <div className="activity">
+    <div className="activity" style= {{backgroundColor: bgColors[id]}}>
       <img className="icon" src={icons[id]} alt="activity" />
       <div className="work">
         <div className="title">
           <h2>{title}</h2>
-          <img src="./images/icon-ellipsis.svg" alt="icon-ellipsis" />
+          <img src={ellipsis} alt="icon-ellipsis" />
         </div>
         <div className="content">
           <h3>{current}hrs</h3>
