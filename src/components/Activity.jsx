d966ddd39a19ebf5b0f8813c88c9b28a9fc32c50
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-16 12:26:53 +0300

    Testing json output

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
-export default function Activity({title}){
-    return <div className="activity">
-    <img className="icon" src="./images/icon-work.svg" alt="icon-briefcase" />
-        <div className="work">
-            <div className="title">
-                <h2>{title}</h2>
-                <img src="./images/icon-ellipsis.svg" alt="icon-ellipsis" />
-            </div>
-            <div className="content">
-                <h3>32hrs</h3>
-                <p>Last Week - 36hrs</p>
-            </div>
+
+export default function Activity({title, current, previous}) {
+  return (
+    <div className="activity">
+      <img
+        className="icon"
+        src="./images/icon-work.svg"
+        alt="icon-briefcase"
+      />
+      <div className="work">
+        <div className="title">
+          <h2>{title}</h2>
+          <img src="./images/icon-ellipsis.svg" alt="icon-ellipsis" />
         </div>
+        <div className="content">
+          <h3>{current}</h3>
+          <p>Last Week - {previous}hrs</p>
+        </div>
+      </div>
     </div>
+  );
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,21 @@ import Heading from "./Heading";
 import Activity from "./Activity";
 import data from "../data.json";
 
- data = [data];
- 
+//  console.log(newData);
 export default function App() {
   return (
     <div className="container">
       <Heading />
-      {data.map((item, index)=>{
-        return <Activity key={index} title={item[index].title}/>
+      {data.map((item, index) => {
+        return (
+          <Activity
+            key={index}
+            id={index}
+            title={item.title}
+            current={item.timeframes.weekly.current}
+            previous={item.timeframes.weekly.previous}
+          />
+        );
       })}
     </div>
   );
